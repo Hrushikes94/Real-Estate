921ad1965e6c5e31a76ac5de2c93e8fa4434af01
Hrushikesh <hrushikesh@studioin.collab>
2026-07-16 13:58:59 +0530

    Configure-basePath-realestate-and-push-rebuilt-static-frontend-assets-to-git

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/realestate',
   images: {
     unoptimized: true,
     remotePatterns: [
